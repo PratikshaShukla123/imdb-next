1c6b5b78175ec76e47253364961252aa1b5772e5
PratikshaShukla123 <146610977+PratikshaShukla123@users.noreply.github.com>
2024-04-29 00:11:01 +0530

    add header section

--- imdb-next/src/components/Header.jsx
+++ imdb-next/src/components/Header.jsx
@@ -11,11 +11,11 @@ export default function Header() {
             <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
         </div>
         <Link href={'/'} className='flex gap-1 items-center'>
-          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
+          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+            IMDb
+            </span>
           <span className='text-xl hidden sm:inline'>Clone</span>
-
-        </Link>
-        
+        </Link>   
     </div>
   );
 }
